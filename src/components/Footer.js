fcae576d5adcba7d149456398172153e7f8e477f
WillMarcuss <williammarcus@bell.net>
2024-07-08 11:17:28 -0400

    updated css to make it more modern, and added fixed position for header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import styles from './Footer.module.css';
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <p>&copy; 2024 My Portfolio</p>
+            <p>&copy; 2024 William Marcus Portfolio</p>
         </footer>
     );
 };
