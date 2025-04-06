2fe0095fe3a9383a5b582d03454204e3b60cf0c7
WillMarcuss <williammarcus@bell.net>
2025-04-06 16:14:43 -0400

    Updated Contact page to be more modern

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,30 +1,35 @@
-// src/pages/Contact.js
-
 import React from 'react';
-import styles from './Contact.module.css'; // Adjust the import path according to your project structure
+import styles from './Contact.module.css';
 
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Contact Me</h1>
-      <div className={styles.contactInfo}>
-        <p><strong>Email:</strong> Williammarcus@cmail.carleton.ca</p>
-        <p><strong>Phone:</strong> 613-220-4407</p>
-        <p><strong>Location:</strong> Ottawa, Ontario</p>
+      <div className={styles.overlay}>
+        <h1 className={styles.heading}>Get in Touch</h1>
+        <div className={styles.contactInfo}>
+          <p><strong>Email:</strong> Williammarcus@cmail.carleton.ca</p>
+          <p><strong>Phone:</strong> 613-220-4407</p>
+          <p><strong>Location:</strong> Ottawa, Ontario</p>
+        </div>
+        <div className={styles.links}>
+          <a
+            href="/resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.linkButton}
+          >
+            View My Resume
+          </a>
+          <a
+            href="https://www.linkedin.com/in/wmarcus1/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.linkButton}
+          >
+            Visit My LinkedIn
+          </a>
+        </div>
       </div>
-      <p>If you would like to view my resume, please view it below:</p>
-      <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className={styles.resumeLink}>
-        View My Resume
-      </a>
-      <p>You can also find me on LinkedIn:</p>
-      <a 
-        href="https://www.linkedin.com/in/wmarcus1/" 
-        target="_blank" 
-        rel="noopener noreferrer" 
-        className={styles.linkedinLink}
-      >
-        Visit My LinkedIn Profile
-      </a>
     </div>
   );
 };
