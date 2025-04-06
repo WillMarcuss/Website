7488292ed845a24507d5b582a945dae041023de6
WillMarcuss <williammarcus@bell.net>
2025-04-06 16:16:40 -0400

    Updated background colour to all pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,7 @@ import styles from './Footer.module.css';
 
 const Footer = () => {
     return (
-        <footer className={styles.footer}>
-            <p>&copy; 2024 William Marcus Portfolio</p>
-        </footer>
+<></>
     );
 };
 
